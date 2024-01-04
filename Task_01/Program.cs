396a4f27d4382ad6ef1b8b4dc5c890eb87796ebc
Denis Lodygin <Alirka94@mail.ru>
2024-01-04 23:41:29 +0500

    Добавили ввод массива

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -4,9 +4,20 @@
     {
         static void Main()
         {
-            Console.WriteLine("Привет");
+            Console.Write("Введите элементы массива строк через пробел: ");
+            string input = Console.ReadLine();
+            string[] originalArray = input.Split(' ');
 
+            string[] newArray = FilterStrings(originalArray, 3);
+            
+
+            Console.WriteLine("Исходный массив строк:");
+            PrintArray(originalArray);
+
+            Console.WriteLine("\nНовый массив строк (длинан <= 3 символа):");
+            PrintArray(newArray);
         }
     }
+    
     
 }
