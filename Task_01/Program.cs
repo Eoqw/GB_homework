08fb05e86ffc7b8369d76f397ada2a6d7823d35d
Denis Lodygin <Alirka94@mail.ru>
2024-01-05 00:36:41 +0500

    Создаем новый массив с учетом найденного количества строк и Заполняем новый массив подходящими строками

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -29,6 +29,26 @@
                 count++;
             }
         }
-    }
     
+    string[] resultArray = new string[count];
+
+    interface index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= maxLenght)
+        {
+            resultArray[index] = array[i];
+            index++;
+        }
+    }
+
+    return resultArray;
+
+    }
 }
+
+        
+
+    
+
+
